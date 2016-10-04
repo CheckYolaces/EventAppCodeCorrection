5127a4dfa5f6e0bb32affabc4facc8caca3cf25e
tevistumulty <tevis.tumulty@west-mec.org>
2016-10-04 11:05:20 -0700

    Update event.js

--- EventApp/js/event.js
+++ EventApp/js/event.js
@@ -1,5 +1,7 @@
 // event.js
 // This script validates two dates and calculates the number of days between them.
+// It also contains a number of Javascript and/or reference errors.  Please go through the code
+// and try to find and correct them.
 
 // Function called when the form is submitted.
 // Function processes the dates and returns false.
